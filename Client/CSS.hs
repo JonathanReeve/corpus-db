6c6716f04c21d06eef7d00d1dc903991138ff4a8
Jonathan Reeve <jon.reeve@gmail.com>
2018-02-07 16:15:01 -0500

    add CSS

---
+++ Client/CSS.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Client.CSS
+    ( layoutCss
+    ) where
+
+import           Clay
+import           Data.Text.Lazy (Text)
+
+layoutCss :: Text
+layoutCss = render $ do
+  body ? minHeight (px 2000)
+  element ".jumbotron" ? (textAlign $ alignSide sideCenter)
+  element "#gmail" ? (marginLeft $ px 10)
