f5440d9141795ae25a558b432de7c4d014b54973
Jonathan Reeve <jon.reeve@gmail.com>
2018-02-08 09:20:36 -0500

    larger font for nav bar

--- Client/CSS.hs
+++ Client/CSS.hs
@@ -4,11 +4,13 @@ module Client.CSS
     ( layoutCss
     ) where
 
-import           Clay
-import           Data.Text.Lazy (Text)
+import Clay
+import Prelude hiding (rem)
+import Data.Text.Lazy (Text)
 
 layoutCss :: Text
 layoutCss = render $ do
   body ? minHeight (px 2000)
   element ".jumbotron" ? (textAlign $ alignSide sideCenter)
   element "#gmail" ? (marginLeft $ px 10)
+  element ".nav" ? (fontSize $ rem 1.5)
