ca4ba9195576f830cfae1c3bdc3f0e40c0502bde
Jonathan Reeve <jon.reeve@gmail.com>
2018-02-15 22:13:54 -0500

    non-working attempt to integrate in the DB layer

--- src/Main.hs
+++ src/Main.hs
@@ -1,49 +1,46 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Main where
 
 import Data.Monoid ((<>))
-import Web.Scotty
+import Database.HDBC
+import Database.HDBC.Sqlite3
+import Data.Convertible.Base
 import Data.Aeson (FromJSON, ToJSON)
 import GHC.Generics
-import           Control.Applicative                  ((<$>))
-import           Controllers.Home                     (home, docs, login)
-import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
-import           Network.Wai.Middleware.Static        (addBase, noDots,
-                                                       staticPolicy, (>->))
-import           System.Environment                   (getEnv)
-import           Web.Scotty                           (middleware, scotty)
+import Control.Applicative                  ((<$>))
+import Controllers.Home                     (home, docs, login)
+import Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import Network.Wai.Middleware.Static        (addBase, noDots,
+                                             staticPolicy, (>->))
+import Web.Scotty
 
-data User = User { userId :: Int, userName :: String } deriving (Show, Generic)
+db :: String
+db = "/home/jon/Code/gitenberg-scrape/pg-text-7.db"
 
-bob :: User
-bob = User { userId = 1, userName = "bob" }
+getPerson :: (Convertible a SqlValue, IConnection conn) => conn -> a -> IO [[SqlValue]]
+getPerson conn person =
+  quickQuery' conn "select title, id from meta where author like ?" [toSql person]
 
-jenny :: User
-jenny = User { userId = 2, userName = "jenny" } 
-
-allUsers :: [User]
-allUsers = [bob, jenny]
-
-instance ToJSON User
-instance FromJSON User
-
-matchesId :: Int -> User -> Bool
-matchesId id user = userId user == id
+getByID :: (Convertible String SqlValue, IConnection conn) =>
+           conn -> String -> IO (Maybe [(String, SqlValue)])
+getByID conn bookID = do
+  stmt <- prepare conn "select * from meta where id = ?"
+  _ <- execute stmt [toSql bookID]
+  fetchRowAL stmt
 
 main :: IO ()
 main = do
   putStrLn "Starting Server..."
-  scotty 80 $ do
+  scotty 8000 $ do
     get "/api/hello/:name" $ do
       name <- param "name"
       text ("hello " <> name <> "!")
-    get "/api/users" $ do
-      json allUsers
-    get "/api/users/:id" $ do
-      id <- param "id"
-      json (filter (matchesId id) allUsers)
+    get "/api/id" $ do
+      conn <- connectSqlite3 db
+      meta <- getByID conn "9.0"
+      json meta
     middleware $ staticPolicy (noDots >-> addBase "static/images") -- for favicon.ico
     middleware logStdoutDev
     home >> docs >> login
