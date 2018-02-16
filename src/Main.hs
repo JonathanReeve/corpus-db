2838c1e6c8083f403f5a4246d46c35c03ca65e04
Jonathan Reeve <jon.reeve@gmail.com>
2018-02-16 14:56:25 -0500

    still non-working attempt to integrate DB layer

--- src/Main.hs
+++ src/Main.hs
@@ -3,11 +3,14 @@
 
 module Main where
 
+import Control.Monad.Trans.Class (lift)
+import Data.Map (fromList)
 import Data.Monoid ((<>))
 import Database.HDBC
 import Database.HDBC.Sqlite3
 import Data.Convertible.Base
-import Data.Aeson (FromJSON, ToJSON)
+import Data.Aeson (FromJSON, ToJSON, fromJSON, toJSON, encode)
+import Data.Maybe
 import GHC.Generics
 import Control.Applicative                  ((<$>))
 import Controllers.Home                     (home, docs, login)
@@ -19,16 +22,31 @@ import Web.Scotty
 db :: String
 db = "/home/jon/Code/gitenberg-scrape/pg-text-7.db"
 
-getPerson :: (Convertible a SqlValue, IConnection conn) => conn -> a -> IO [[SqlValue]]
+-- getPerson :: (Convertible a SqlValue, IConnection conn) => conn -> a -> IO [[SqlValue]]
 getPerson conn person =
   quickQuery' conn "select title, id from meta where author like ?" [toSql person]
 
-getByID :: (Convertible String SqlValue, IConnection conn) =>
-           conn -> String -> IO (Maybe [(String, SqlValue)])
-getByID conn bookID = do
+-- getByID :: (Convertible String SqlValue, IConnection conn) =>
+--            conn -> String -> IO (Maybe [(String, SqlValue)])
+getByID bookID = do
+  conn <- connectSqlite3 db
   stmt <- prepare conn "select * from meta where id = ?"
   _ <- execute stmt [toSql bookID]
   fetchRowAL stmt
+
+sqlToText :: Maybe [(String, SqlValue)] -> Maybe [(String, String)]
+sqlToText sqlPairList = case sqlPairList of
+  Nothing -> Nothing
+  Just sqlPairList -> Just $ map getVal sqlPairList where
+    getVal (a, val) = case val of SqlNull -> (a, "NULL")
+                                  otherwise -> (a, fromSql val :: String)
+
+-- textToJson :: Maybe [(String, String)] -> String
+textToJson pairList = case pairList of
+  Nothing -> ""
+  Just pairList -> do
+    let myMap = fromList pairList
+    toJSON myMap
 
 main :: IO ()
 main = do
@@ -38,9 +56,14 @@ main = do
       name <- param "name"
       text ("hello " <> name <> "!")
     get "/api/id" $ do
-      conn <- connectSqlite3 db
-      meta <- getByID conn "9.0"
+      sql <- getByID "9.0"
+      text <- sqlToText sql
+      meta <- textToJson text
       json meta
+      -- meta <- getByID "9.0"
+      -- text $ lift meta
+      -- json meta
+      -- text "hello"
     middleware $ staticPolicy (noDots >-> addBase "static/images") -- for favicon.ico
     middleware logStdoutDev
     home >> docs >> login
