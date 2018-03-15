a0181d3420ac03179ca79d1c5cf0649dcec275b8
Jonathan Reeve <jon.reeve@gmail.com>
2018-03-15 17:49:54 -0400

    non-working attempt at XML parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import Data.Maybe
+import qualified Data.Text.IO as TIO
+import qualified Data.Text as T
+import Text.XML.Light
+
+testText = "../gutenberg-meta/rdf-files/cache/epub/10885/pg10885.rdf"
+
+rdfqn = QName "RDF" (Just "http://www.w3.org/1999/02/22-rdf-syntax-ns#") (Just "rdf")
 
 main :: IO ()
 main = do
   putStrLn "hello world"
+  rdf <- TIO.readFile testText
+  let xml = parseXML rdf
+      elems = onlyElems xml
+      rdfElems = map (findElement rdfqn) elems
+      rdf = filter isJust rdfElems
+  print rdf
