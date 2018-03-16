f79c901076c9ad650168e63a5732ec41e80ffd7b
Jonathan Reeve <jon.reeve@gmail.com>
2018-03-15 23:34:03 -0400

    working attempt to get namespaces out of RDF

--- src/Main.hs
+++ src/Main.hs
@@ -6,17 +6,29 @@ import Data.Maybe
 import qualified Data.Text.IO as TIO
 import qualified Data.Text as T
 import Text.XML.Light
+import qualified Data.Map as M
 
 testText = "../gutenberg-meta/rdf-files/cache/epub/10885/pg10885.rdf"
 
 rdfqn = QName "RDF" (Just "http://www.w3.org/1999/02/22-rdf-syntax-ns#") (Just "rdf")
 
+type NamespaceMap = M.Map String String
+
+mkNamespaces :: [Attr] -> NamespaceMap
+mkNamespaces attrs = M.fromList $ map mkNamespace (filter isNamespace attrs)
+  where isNamespace attr = qPrefix (attrKey attr) == Just "xmlns"
+        mkNamespace attr = (qName (attrKey attr), attrVal attr)
+
+getRoot xml = do
+      let parsed = parseXML xml
+          elems = onlyElems parsed
+      head $ mapMaybe (findElement rdfqn) elems
+
 main :: IO ()
 main = do
   putStrLn "hello world"
   rdf <- TIO.readFile testText
-  let xml = parseXML rdf
-      elems = onlyElems xml
-      rdfElems = map (findElement rdfqn) elems
-      rdf = filter isJust rdfElems
-  print rdf
+  let root = getRoot rdf
+      attrs = elAttribs root
+      ns = mkNamespaces attrs
+  print ns
