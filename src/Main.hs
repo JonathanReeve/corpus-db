0e7882e2b822436039463878fc3ab9d4fb03ca40
Jonathan Reeve <jon.reeve@gmail.com>
2018-03-23 10:17:36 -0400

    parse RDF using RDF4H library instead of raw XML

--- src/Main.hs
+++ src/Main.hs
@@ -1,34 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
-import Data.Maybe
-import qualified Data.Text.IO as TIO
-import qualified Data.Text as T
-import Text.XML.Light
-import qualified Data.Map as M
+import Data.RDF
 
+testText :: FilePath
 testText = "../gutenberg-meta/rdf-files/cache/epub/10885/pg10885.rdf"
 
-rdfqn = QName "RDF" (Just "http://www.w3.org/1999/02/22-rdf-syntax-ns#") (Just "rdf")
-
-type NamespaceMap = M.Map String String
-
-mkNamespaces :: [Attr] -> NamespaceMap
-mkNamespaces attrs = M.fromList $ map mkNamespace (filter isNamespace attrs)
-  where isNamespace attr = qPrefix (attrKey attr) == Just "xmlns"
-        mkNamespace attr = (qName (attrKey attr), attrVal attr)
-
-getRoot xml = do
-      let parsed = parseXML xml
-          elems = onlyElems parsed
-      head $ mapMaybe (findElement rdfqn) elems
-
+parseRDFXML :: String -> IO (Either ParseFailure (RDF TList))
+parseRDFXML = parseFile (XmlParser Nothing Nothing)
+  
 main :: IO ()
 main = do
+  parsed <- parseRDFXML testText
   putStrLn "hello world"
-  rdf <- TIO.readFile testText
-  let root = getRoot rdf
-      attrs = elAttribs root
-      ns = mkNamespaces attrs
-  print ns
+  print parsed
