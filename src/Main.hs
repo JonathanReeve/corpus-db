3b048a8e472065796e30e0e8f7b80f4f14722dfe
Jonathan Reeve <jon.reeve@gmail.com>
2018-03-29 18:53:14 -0400

    messy but working attempt to integrate Persistent a little
    uses titles and Gutenberg IDs at the moment

--- src/Main.hs
+++ src/Main.hs
@@ -12,6 +12,7 @@ module Main where
 
 import Data.RDF
 import qualified Data.Text as T (Text, splitOn, unpack)
+import Data.Text.Read (decimal)
 import System.FilePath.Glob (glob)
 
 import           Control.Monad.IO.Class  (liftIO)
@@ -19,35 +20,27 @@ import           Database.Persist
 import           Database.Persist.Sqlite
 import           Database.Persist.TH
 
+-- data Book = Book {id :: BookID,
+--                   titles :: [Title],
+--                   authors :: [AuthorID],
+--                   tocs :: [TOC]} deriving Show
+
+-- type AuthorID = Int
+-- type Title = T.Text
+-- type BookID = Int
+-- type TOC = T.Text
+
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
-Person
+Book
+    gutId Int
+    title [T.Text]
+    toc [T.Text] Maybe
+    deriving Show
+Author
+    gutId Int
     name String
-    age Int Maybe
-    deriving Show
-BlogPost
-    title String
-    authorId PersonId
-    deriving Show
 |]
 
-db :: IO ()
-db = runSqlite "test.db" $ do
-    runMigration migrateAll
-
-    johnId <- insert $ Person "John Doe" $ Just 35
-    janeId <- insert $ Person "Jane Doe" Nothing
-
-    insert $ BlogPost "My fr1st p0st" johnId
-    insert $ BlogPost "One more for good measure" johnId
-
-    oneJohnPost <- selectList [BlogPostAuthorId ==. johnId] [LimitTo 1]
-    liftIO $ print (oneJohnPost :: [Entity BlogPost])
-
-    john <- get johnId
-    liftIO $ print (john :: Maybe Person)
-
-    -- delete janeId
-    -- deleteWhere [BlogPostAuthorId ==. johnId]
 
 testText :: FilePath
 testText = "../gutenberg-meta/rdf-files/cache/epub/10885/pg10885.rdf"
@@ -78,21 +71,26 @@ getURIs rdf verb = objectURIs where
 getTriples :: RDF TList -> T.Text -> Triples
 getTriples rdf verb = query rdf Nothing (Just (UNode verb)) Nothing
 
-getTitles :: RDF TList -> [Title]
+-- getTitles :: RDF TList -> [BookTitle]
 getTitles rdf = getPlains rdf "dcterms:title"
 
-getTOCs :: RDF TList -> [TOC]
+-- getTOCs :: RDF TList -> [BookToc]
 getTOCs rdf = getPlains rdf "dcterms:tableOfContents"
 
-getAuthors :: RDF TList -> [AuthorID]
-getAuthors rdf = map (parseAuthor . getURIpath) $ getURIs rdf "dcterms:creator" where
-  parseAuthor rawAuthor = read $ T.unpack rawAuthor
+-- getAuthors :: RDF TList -> [BookAuthorID]
+-- getAuthors rdf = map (parseAuthor . getURIpath) $ getURIs rdf "dcterms:creator" where
+--   parseAuthor rawAuthor = read $ T.unpack rawAuthor
+
+-- getAuthors rdf = map (parseAuthor . getURIpath) $ getURIs rdf "dcterms:creator" where
+--   parseAuthor rawAuthor = case (decimal rawAuthor) of
+--     (Right num) -> fst num
+--     (Left _)    -> error "Couldn't parse the author ID." 
 
 -- | Takes a path, like http://some-URL-here.com/path/to/the-thing and returns the-thing
 getURIpath :: T.Text -> T.Text
 getURIpath uri = last $ T.splitOn "/" uri
 
-getID :: RDF TList -> BookID
+-- getID :: RDF TList -> BookGutId
 getID rdf = bookID where
   triples = query rdf Nothing Nothing (Just (UNode "pgterms:ebook"))
   ids = map (extractID . subjectOf) triples
@@ -103,25 +101,29 @@ getID rdf = bookID where
     [x] -> read $ T.unpack x
     _ -> error "No IDs or something wrong with the ID."
 
-data Book = Book {id :: BookID,
-                  titles :: [Title],
-                  authors :: [AuthorID],
-                  tocs :: [TOC]} deriving Show
-
-type AuthorID = Int
-type Title = T.Text
-type BookID = Int
-type TOC = T.Text
-
-testFile file = do
-  parsed <- parseRDFXML file
-  let result = fromEither parsed
-      testBook = Book (getID result) (getTitles result) (getAuthors result) (getTOCs result)
-  print testBook
-
 rdfFiles :: IO [FilePath]
 rdfFiles = glob "../gutenberg-meta/**/*.rdf"
 
 main :: IO ()
 main = do
-  testFile testText2
+  parsed <- parseRDFXML testText
+  let result = fromEither parsed
+      testBook = Book (getID result) (getTitles result) (Just (getTOCs result))
+  print testBook
+  runSqlite "test.db" $ do
+    runMigration migrateAll
+
+    testBookId <- insert $ testBook
+    -- johnId <- insert $ Person "John Doe" $ Just 35
+    -- janeId <- insert $ Person "Jane Doe" Nothing
+
+    -- insert $ BlogPost "My fr1st p0st" johnId
+    -- insert $ BlogPost "One more for good measure" johnId
+
+    -- oneJohnPost <- selectList [BlogPostAuthorId ==. johnId] [LimitTo 1]
+    -- liftIO $ print (oneJohnPost :: [Entity BlogPost])
+
+    -- john <- get johnId
+    -- liftIO $ print (john :: Maybe Person)
+    testBookResult <- selectList [BookId ==. testBookId] [LimitTo 1]
+    liftIO $ print testBookResult
